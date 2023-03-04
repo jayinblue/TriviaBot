00c3ff235f36d5d16aed25994938595b7708ecf5
Jay <88851187+jayinblue@users.noreply.github.com>
2023-03-04 00:47:43 +0000

    Initial commit

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Lists all of the commands Cordelia has!'),
+	async execute(interaction) {
+		const helpEmbed = new EmbedBuilder()
+			.setColor('Aqua')
+			.setTitle('**Trivia\'s Command List**')
+			.setDescription('L you thought there was something here?')
+			.setTimestamp()
+
+	await interaction.reply({ embeds: [helpEmbed] });
+	},
+};
